1aa0f44bbe7a3c122111637a91192152094b4ac5
Kulcsar Kalman <kalman.kulcsar@lynxsolutions.eu>
2024-04-11 13:49:18 +0300

    OOP basics

--- index.php
+++ index.php
@@ -1,5 +1,67 @@
 <?php
 
-echo("Hello world");
+class Fruit
+{
+    public const CLASS_DESCRIPTION = "here goes a short description about the fruit class";
 
-echo("Hi again");
+    public $name;
+
+    private $color;
+
+    public function __construct(string $ourCustomName, string $ourCustomColor)
+    {
+        $this->name = $ourCustomName;
+        $this->color = $ourCustomColor;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+}
+
+interface FruitInterface
+{
+    public function getSomeProperty();
+
+    public function setSomeProperty(string $name);
+}
+
+class Apple extends Fruit implements FruitInterface
+{
+    protected $size;
+
+    public function __construct(
+        string $name,
+        string $color,
+        int $size = 0
+    ) {
+        parent::__construct($name, $color);
+
+        $this->size = $size;
+    }
+
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    public function getSomeProperty()
+    {
+        return $this->name;
+    }
+
+    public function setSomeProperty(string $name)
+    {
+        $this->name = $name;
+    }
+}
+
+$apple = new Apple("Apple", "Blue");
+
+echo $apple->name;
+echo "<br>";
+
+$apple->setSomeProperty("Banana");
+
+echo $apple->name;
